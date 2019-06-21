ca39a8bc8da6ea0b43fa6bd8ce0c73522487320d
Danielle Martain <dmartain@gmail.com>
2019-06-21 18:12:33 -0500

    jquery-salary-calculator

--- client.js
+++ client.js
@@ -1,1 +1,47 @@
-console.log('jQuery Salary Calculator');
+$( document ).ready( onReady );
+
+let input = []; 
+
+function onReady() {
+    console.log('Submit Button');
+    
+    $( '#addInfoButton' ).on( 'click', addInfo);
+}
+
+function addInfo() {
+    // capture user input
+    // place in a new object
+    let addEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idNumber: $('#idNumberIn').val(),
+        title: $('#titleIn').val(),
+        annualSalary: $('#annualSalaryIn').val()
+    }
+        console.log( 'adding:', addEmployee );
+        // push object into array
+        input.push( addEmployee );
+        // display inputs
+        showEmployeeInfo();
+       
+
+}
+function addTaskHandler(event) {
+    event.preventDefault();
+}
+
+function showEmployeeInfo(){
+    let el = $( '#employeeOut' );
+    // empty our input element
+    el.empty();
+    
+    // loop through Employee info
+for( let i = 0; i<input.length; i++ ) {
+    // append each employee to the DOM
+    el.append( `<li>${input[i].firstName}</li>` );
+    el.append(`<li>${input[i].lastName}</li>`);
+    el.append(`<li>${input[i].idNumber}</li>`);
+    el.append(`<li>${input[i].title}</li>`);
+    el.append(`<li>${input[i].annualSalary}</li>`);
+} // end for
+}
